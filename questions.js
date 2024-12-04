09886f6be3204009036731ebc3cd8c3da4bed757
Mindasandhya <137989896+Mindasandhya@users.noreply.github.com>
2024-12-04 11:27:13 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "what language of javascript?",
+    answer: "Dynamic Programming",
+    options: [
+      "Static programming",
+      "Dynamic Programming",
+      "Hypertext programming",
+      "esay programming"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does DBMS stand for?",
+    answer: "Database Management system",
+    options: [
+      "Database Management system",
+      "Database manage system",
+      "databaise manager system",
+      "Databaise management sys"
+    ]
+  },
+];
